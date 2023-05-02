b63064125f878951513d0400bc33d8a075383b12
Clement <ckhorsimino@gmail.com>
2023-05-02 16:09:42 +0200

    modification renderer

---
+++ contactListApp/src/renderer.js
@@ -0,0 +1,8 @@
+let btnExp = document.getElementById("exp");
+let btnExpList = document.getElementById("expList");
+let btnImp = document.getElementById("imp");
+let btnAdd = document.getElementById("add");
+
+btnAdd.addEventListener("click", () => {
+  monapi.createSecondWindows("contact.html");
+});
